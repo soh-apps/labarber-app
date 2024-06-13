d7f6d4dd16e1e1ade1c56c0b0cf24c8532d868e3
Lucas <lsantana.raro@lidercap.com.br>
2024-06-12 22:29:39 -0300

    Feature: adicionando utils e formatters

--- lib/core/di/di.dart
+++ lib/core/di/di.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:la_barber/core/local_secure_storage/local_secure_storage.dart';
 import 'package:la_barber/features/admin/barbershop/presentation/cubit/barbershop_cubit.dart';
 import 'package:la_barber/features/admin/barbershop/repository/barbershop_repository.dart';
 import 'package:la_barber/core/local_secure_storage/local_secure_storage.dart';
@@ -13,14 +14,17 @@ final GetIt getIt = GetIt.instance;
 
 Future<void> configureInjection() async {
   getIt.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
-  getIt.registerLazySingleton<LocalSecureStorage>(() => LocalSecureStorage());
 
   // RestCLient
   getIt.registerLazySingleton<RestClient>(() => RestClient());
 
   // Repositories
   getIt.registerLazySingleton<AuthRepository>(() => AuthRepository(restClient: getIt<RestClient>()));
+  getIt.registerLazySingleton<BarbershopRepository>(() => BarbershopRepository(restClient: getIt<RestClient>()));
+
+  // Cubits
   getIt.registerLazySingleton<AuthCubit>(() => AuthCubit(getIt<AuthRepository>(), getIt<LocalSecureStorage>()));
+  getIt.registerFactory<BarbershopCubit>(() => BarbershopCubit(getIt<BarbershopRepository>()));
 
   await getIt.allReady();
 
